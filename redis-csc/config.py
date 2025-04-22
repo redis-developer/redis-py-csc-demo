3c8a3b2b1551c1bf6a8b2c1e2b74d67d74d2ef61
dmaier-redislabs <david.maier@redis.com>
2025-04-22 18:20:33 +0200

    Added some boilerplate

---
+++ redis-csc/config.py
@@ -0,0 +1,5 @@
+DB_CONFIG = {
+    'host': '<Your host here>',
+    'port': 6379,
+    'password' : 'Your password here',
+}
---
+++ redis-csc/simple_demo.py
@@ -0,0 +1,10 @@
+from redis import Redis
+
+
+def simple_demo():
+
+
+
+
+if __name__ == "__main__":
+    simple_demo()
