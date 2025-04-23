95643ab1c4070f193eb6a1031b644d5bdee998ec
dmaier-redislabs <david.maier@redis.com>
2025-04-23 14:50:55 +0200

    Added multi-threading

--- redis-csc/perf_demo.py
+++ redis-csc/perf_demo.py
@@ -1,10 +1,20 @@
 from config import *
-from tqdm import tqdm
 import time
+import threading
+from enum import Enum
 from redis import Redis
 from redis.cache import CacheConfig
 import random
 import string
+
+
+''' 
+Available workloads
+'''
+class Workload(Enum):
+    OTHER_WORKLOAD = 0
+    SEQUENTIAL_WR_1_TO_2 = 1
+
 
 '''
 Generate a random string of a given length
@@ -33,6 +43,25 @@ def warmup(r, num_keys, value_size):
         r.set("key:{}".format(i), random_string(value_size))
 
 
+
+def run_workload(workload, num_workers, r_write, r_read, num_keys, value_size):
+    workers = []
+    keys_per_thread = int(num_keys / num_workers)
+    keys_rest = num_keys - (num_keys * num_workers)
+
+    for worker_id in range(num_workers):
+        num_keys = keys_per_thread
+        if worker_id == num_workers - 1:
+            num_keys += keys_rest
+
+        if workload.value == Workload.SEQUENTIAL_WR_1_TO_2.value:
+            worker = threading.Thread(target=run_sequential_workload_1_to_2, args=(worker_id, r_write, r_read, num_keys, value_size))
+            workers.append(worker)
+            worker.start()
+
+    # Wait until all workers are done
+    for w in workers:
+        w.join()
 '''
 Mix some reads and writes with a ratio of 1:2
 
@@ -40,11 +69,13 @@ Mix some reads and writes with a ratio of 1:2
 2. Perform a write to a String
 3. Perform two read operations on the same String££
 '''
-def run_sequential_workload_1_to_2(r_write, r_read, num_keys, value_size):
-    for i in tqdm(range(num_keys), desc="Running workload"):
-        r_write.set("key:{}".format(i), random_string(value_size))
-        r_read.get("key:{}".format(i))
-        r_read.get("key:{}".format(i))
+def run_sequential_workload_1_to_2(worker_id, r_write, r_read, num_keys, value_size):
+    for i in range(num_keys):
+        r_write.set("key:{}:{}".format(worker_id, i), random_string(value_size))
+        r_read.get("key:{}:{}".format(worker_id, i))
+        r_read.get("key:{}:{}".format(worker_id, i))
+
+
 
 
 if __name__ == "__main__":
@@ -52,18 +83,23 @@ if __name__ == "__main__":
     NUM_KEYS = 10000
     VALUE_SIZE = 50
     CACHE_SIZE = 1000
+    NUM_THREADS = 20
+    WORKLOAD = Workload.SEQUENTIAL_WR_1_TO_2
 
+    r_write = Redis(host=DB_CONFIG['host'], port=DB_CONFIG['port'], password=DB_CONFIG['password'])
     r_uncached = Redis(host=DB_CONFIG['host'], port=DB_CONFIG['port'], password=DB_CONFIG['password'])
     r_cached = Redis(host=DB_CONFIG['host'], port=DB_CONFIG['port'], password=DB_CONFIG['password'],
                         protocol=3, cache_config=CacheConfig(max_size=CACHE_SIZE))
 
-    print("Running the workload with an uncached connection ...")
     r_uncached.flushdb()
-    stopwatch(run_sequential_workload_1_to_2, r_uncached, r_uncached, NUM_KEYS, VALUE_SIZE)
+    print("Running the workload with an uncached connection ...")
+    stopwatch(run_workload, WORKLOAD, NUM_THREADS, r_write, r_uncached, NUM_KEYS, VALUE_SIZE)
+
+    print("Enabling caching ...")
+    time.sleep(5)
 
     print("Running the workload with a cached connection ...")
-    r_uncached.flushdb()
-    stopwatch(run_sequential_workload_1_to_2, r_uncached, r_cached, NUM_KEYS, VALUE_SIZE)
+    stopwatch(run_workload, WORKLOAD, NUM_THREADS, r_uncached, r_cached, NUM_KEYS, VALUE_SIZE)
 
 
 
